224a0bc58dbe10254160378d6134ecc9f5c88a3e
OwenHoffend <owenhoffend@gmail.com>
2024-01-16 16:34:54 -0500

    hypergeometric variance test

--- experiments/et_variance.py
+++ experiments/et_variance.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sim.PTM import get_vin_mc0, get_vin_mc1, get_vin_mcn1
-from sim.Util import avg_loop, array_loop
+from sim.Util import avg_loop, array_loop, clog2
 
-def stupid_var_test(num_trials, N):
-    p = np.random.uniform()
-    var = p * (1-p) / N
+def bitstream_var_test_binomial(num_trials, N):
+    N1 = np.random.randint(0, N)
+    p = N1 / N
+    var = (p * (1-p)) / N
     MSE = 0
     for i in range(num_trials):
         out = 0.0
@@ -17,32 +18,84 @@ def stupid_var_test(num_trials, N):
     print("MSE: ", MSE)
     print("var: ", var)
 
+def bitstream_var_test_hypergeometric(num_trials, N, nc2):
+    assert N < nc2
+    W1 = np.random.randint(0, nc2)
+    w = W1 / nc2
+    var = (w * (1-w) / N) * ((nc2 - N) / (nc2 - 1))
+    MSE = 0
+    for i in range(num_trials):
+        bs = np.concatenate((np.ones(W1,), np.zeros((nc2-W1,))))
+        np.random.shuffle(bs)
+        out = np.mean(bs[:N])
+        MSE += (out - w) ** 2
+    MSE /= num_trials
+    print("MSE: ", MSE)
+    print("var: ", var)
+
 def get_MSE_binomial(w, vin, N, nv2, num_trials):
     #Helper function to compute the actual output variance
     MSE = 0
     correct = vin.T @ w
-    #order = np.array([x for x in range(nc2)])
-    #np.random.shuffle(order)
     for _ in range(num_trials):
         samples = np.random.choice(nv2, N, p=vin)
         out = 0.0
         for i in range(N):
             ws = w[samples[i]]
-            #if hypergeometric:
-            #    if (order[i] % nc2) / nc2 <= ws:
-            #        out += 1.0
-            #else:
             out += np.random.choice(np.array([0.0, 1.0]), 1, p=np.array([1-ws, ws]))
         out /= N
         MSE += (out - correct) ** 2
     MSE /= num_trials
     return MSE
 
+def get_MSE_hypergeometric(w, vin, N, nv2, nc2, m, num_trials):
+    MSE = 0
+    correct = vin.T @ w
+    var = (correct * (1-correct) / N) * ((m*nc2 - N) / (m*nc2 - 1))
+
+    F = np.zeros((nv2, nc2))
+    for i, weight in enumerate(w):
+        W1 = (weight * nc2).astype(int)
+        F[i, :] = np.concatenate((np.ones(W1,), np.zeros((nc2-W1,))))
+
+
+    vin_ints = vin * m
+    v_inds = []
+    for idx, vi in enumerate(vin_ints):
+        v_inds += [idx, ] * vi.astype(int)
+    v_inds = np.array(v_inds)
+    c_inds = np.array(range(nc2))
+    v, c = np.meshgrid(v_inds, c_inds)
+    v_inds = v.flatten()
+    c_inds = c.flatten()
+
+    assert v_inds.size == m*nc2
+    assert c_inds.size == m*nc2
+
+    trials = np.empty(num_trials)
+    mnc2i = (m*nc2).astype(int)
+    order = np.array(range(mnc2i))
+    for t in range(num_trials):
+        #print(t)
+        out = 0.0
+        np.random.shuffle(order)
+        for i in range(N):
+            o = order[i]
+            vidx = v_inds[o % mnc2i]
+            cidx = c_inds[o % mnc2i]
+            out += F[vidx, cidx]
+
+        out /= N
+        trials[t] = out
+        MSE += (out - correct) ** 2
+    MSE /= num_trials
+    print("MSE: ", MSE)
+    print("actual var: ", np.var(trials))
+    print("model var: ", var)
+    return MSE
+
 def et_var_test(num_trials, N, hypergeometric=False):
-    nv2 = 4
-    nc2 = 4
-    vin = np.random.uniform(size=(nv2,))
-    vin = vin / np.sum(vin)
+
     w = np.random.uniform(0, nc2-1, size=(nv2,)) / nc2
     correct = vin.T @ w
 
@@ -77,6 +130,31 @@ def bayes_biased_coin(bias):
         v_pmf = likelihood.T * v_pmf / evidence
     plt.plot(pmf_vals, v_pmf)
     plt.show()
+
+def get_dist(dist, n):
+    if dist == 'uniform':
+        px_func = lambda: np.random.uniform(size=n)
+    elif dist == 'MNIST':
+        px_func = lambda: sample_from_mnist(np.sqrt(n).astype(int))
+    elif dist == 'MNIST_beta':
+        px_func = lambda: np.random.beta(0.0362, 0.1817, size=n)
+    elif dist == 'center_beta':
+        px_func = lambda: np.random.beta(3, 3, size=n)
+    else:
+        raise ValueError
+    return px_func
+
+def static_et_eval(w, var, num_pxs, dist):
+    #Compute the best N to early terminate at based on dataset statistics
+    nv2 = w.size
+    n = clog2(nv2)
+    px_func = get_dist(dist, n)
+
+    for i in range(num_pxs):
+        pass
+
+def binomial_static_et_test(w, vin, var, num_trials):
+    pass
 
 def binomial_dynamic_et_test(w, vin, var, num_trials, plot=False):
     #Dynamic ET test that seeks to estimate the variable-input PMF by simply summing
@@ -131,18 +209,9 @@ def sample_from_mnist(winsz):
 
 def scc_dynamic_et_test(w, var, num_pxs, num_trials, corr, dist):
     nv2 = w.size
-    n = np.ceil(np.log2(nv2)).astype(int)
+    n = clog2(nv2)
 
-    if dist == 'uniform':
-        px_func = lambda: np.random.uniform(size=n)
-    elif dist == 'MNIST':
-        px_func = lambda: sample_from_mnist(np.sqrt(n).astype(int))
-    elif dist == 'MNIST_beta':
-        px_func = lambda: np.random.beta(0.0362, 0.1817, size=n)
-    elif dist == 'center_beta':
-        px_func = lambda: np.random.beta(3, 3, size=n)
-    else:
-        raise ValueError
+    px_func = get_dist(dist, n)
 
     def inner():
         px = px_func()
@@ -164,32 +233,3 @@ def scc_dynamic_et_test(w, var, num_pxs, num_trials, corr, dist):
                                                                     np.round(np.mean(vals), 3), np.round(np.std(vals), 3)))
     plt.ylim((0, num_pxs))
     plt.show()
-
-#OLD CODE
-#def dynamic_et_test():
-#    nv2 = 4
-#    nc2 = 4
-#    Nmax = 100
-#    vin = np.array([0.5, 0, 0.5, 0])
-#    vin = vin / np.sum(vin)
-#    #w = np.random.uniform(0, nc2-1, size=(nv2,)) / nc2
-#    #correct = vin.T @ w
-#
-#    num_pmf_vals = 20
-#    pmf_vals_1d = np.linspace(0, 1, num_pmf_vals)
-#    
-#    init_uniform = 1.0 / (num_pmf_vals ** nv2)
-#    dims = tuple([num_pmf_vals,] * nv2)
-#    v_pmf = np.full(dims, init_uniform)
-#
-#    for t in range(Nmax):
-#        sample = np.random.choice(nv2, 1, p=vin)[0]
-#        likelihood = pmf_vals_nv2d[sample]
-#        evidence = np.sum(likelihood * v_pmf)
-#        v_pmf = likelihood * v_pmf / evidence
-#
-#    #marginal distribution for the first pmf entry:
-#    plt.plot(pmf_vals_1d, np.sum(v_pmf, axis=(1, 2, 3)))
-#    plt.show()
-
-
--- main.py
+++ main.py
@@ -6,20 +6,20 @@ from sim.circs import *
 from experiments.et_variance import *
 
 #w = np.array([0, 0.5, 0.5, 1]) #MUX/MAJ
-w = get_weight_matrix_from_ptm(get_func_mat(robert_cross, 5, 1), 1, 1, 4)[:, 0]
-print(w)
-var = 0.0001
+#w = get_weight_matrix_from_ptm(get_func_mat(robert_cross, 5, 1), 1, 1, 4)[:, 0]
+#print(w)
+#var = 0.0001
 
-num_pxs = 100
-num_trials = 1
+#num_pxs = 100
+#num_trials = 1
 
 #RCED
-px11 = 0.1
-px12 = 0.1
-px21 = 0.9
-px22 = 0.9
-px = np.array([px11, px12, px21, px22])
-vin = get_vin_mc1(px)
+#px11 = 0.1
+#px12 = 0.1
+#px21 = 0.9
+#px22 = 0.9
+#px = np.array([px11, px12, px21, px22])
+#vin = get_vin_mc1(px)
 
 #MUX
 #px = 0.5
@@ -31,4 +31,18 @@ vin = get_vin_mc1(px)
 #vars = [0.001, 0.0001]
 
 #scc_dynamic_et_test(w, var, num_pxs, num_trials, 1, 'MNIST_beta')
-binomial_dynamic_et_test(w, vin, var, num_trials, plot=True)
+#binomial_dynamic_et_test(w, vin, var, num_trials, plot=True)
+
+#bitstream_var_test_hypergeometric(10000, 16, 1024) #WORKS
+
+#test of hypergeometric variance eq for full circuit
+nv2 = 32
+nc2 = 32
+
+v_denom = 16
+vin = np.random.random_integers(0, v_denom, size=(nv2,))
+m = np.sum(vin)
+vin = vin / m
+w = np.random.random_integers(0, nc2, size=(nv2,)) / nc2
+N = nc2 * m >> 5
+get_MSE_hypergeometric(w, vin, N, nv2, nc2, m, 1000)
